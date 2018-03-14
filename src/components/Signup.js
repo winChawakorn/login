5034057baea7a958907c61be7967f2adb5289f82
winChawakorn <chawakorn.su@ku.th>
2018-03-14 18:00:22 +0700

    re-design signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,30 +4,28 @@ import { Link } from 'react-router-dom';
 class Signup extends React.Component {
     render() {
         return (
-            <div className="signup">
-                <div className="signup-form">
-                    <div style={{ fontSize: "25px", marginTop: "0.5em", marginBottom: "0.5em" }}>SIGN UP</div>
-                    <div style={{ marginLeft: `20px`, textAlign: "left" }}>
-                        <div style={{ overflow: "auto" }}>
-                            <div style={{ float: "left" }}>
-                                <div className="signup-text">Firstname</div>
-                                <input placeholder="Firstname" className="textfield" />
-                            </div>
-                            <div style={{ float: "left" }}>
-                                <div className="signup-text">Lastname</div>
-                                <input placeholder="Lastname" className="textfield" />
-                            </div>
+            <div className="signup-form">
+                <div className="topic">SIGN UP</div>
+                <div style={{ marginLeft: "20px", textAlign: "left" }}>
+                    <div style={{ overflow: "auto" }}>
+                        <div style={{ float: "left", marginRight: "7.5em" }}>
+                            <div className="signup-text">Firstname</div>
+                            <input placeholder="Firstname" className="textfield" />
                         </div>
-                        <div className="signup-text">E-mail</div>
-                        <input placeholder="E-mail" className="textfield" />
-                        <div className="signup-text">Password</div>
-                        <input placeholder="Password" className="textfield" />
-                        <div className="signup-text">Confirm Password</div>
-                        <input placeholder="Confirm Password" className="textfield" />
+                        <div style={{ float: "left" }}>
+                            <div className="signup-text">Lastname</div>
+                            <input placeholder="Lastname" className="textfield" />
+                        </div>
                     </div>
-                    <Link to="/"><button className="signup-btn" style={{ marginTop: "2em" }}>Sign up</button></Link>
-                    <Link to="/"><button className="signup-btn" style={{ marginLeft: "3em", background: "cornflowerblue" }}>Cancel</button></Link>
+                    <div className="signup-text">E-mail</div>
+                    <input placeholder="E-mail" className="textfield" />
+                    <div className="signup-text">Password</div>
+                    <input placeholder="Password" className="textfield" type="password" />
+                    <div className="signup-text">Confirm Password</div>
+                    <input placeholder="Confirm Password" className="textfield" type="password" />
                 </div>
+                <Link to="/"><button className="button" style={{ width: "200px", marginTop: "3em" }}>Sign up</button></Link>
+                <Link to="/"><button className="button" style={{ width: "200px", marginLeft: "5em" }}>Cancel</button></Link>
             </div>
         );
     }
