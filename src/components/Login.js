5c6ec03aca5bebe4ab33c007c6e12bcd5e3279bf
winChawakorn <chawakorn.su@ku.th>
2018-03-14 17:34:36 +0700

    re-design login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,15 +4,18 @@ import { Link } from 'react-router-dom';
 class Login extends React.Component {
     render() {
         return (
-            <div style={{ textAlign: "center", color: "white", marginTop: "150px" }}>
-                <div className="login-form">
-                    <div style={{ fontSize: "25px" }}>SIGN IN</div>
-                    <input placeholder="E-mail address" className="textfield" style={{ width: "300px", textAlign: "left" }} /><br></br>
-                    <input placeholder="Password" className="textfield" style={{ width: "300px", textAlign: "left" }} />
-                    <Link to="/user"><button>Login</button></Link><br></br>
-                    {/* <Link to="/recover">forget your password ?</Link> */}
+            <div className="login-form">
+                <div className="topic">SIGN IN</div>
+                <input placeholder="E-mail address" className="textfield" style={{ width: "300px", textAlign: "left" }} />
+                <input placeholder="Password" className="textfield" type="password" style={{ width: "300px", textAlign: "left" }} />
+                <Link to="/user"><button className="button">Login</button></Link><br></br>
+                {/* <Link to="/recover">forget your password ?</Link> */}
+                <div>
+                    Don't have an account ?
+                    <Link to="/signup">
+                        <button className="button" style={{ width: "100px", padding: "4px", fontSize: "14px" }}>Sign up</button>
+                    </Link>
                 </div>
-                Don't have an account ? <Link to="/signup"><button className="signup-btn" style={{ marginTop: "20px", marginLeft: "10px" }}>Sign up</button></Link>
             </div>
         );
     }
